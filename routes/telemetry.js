554112f7c2cfc190bb1f8059639d800ce93f6933
Tayo Carvalhal <lucarv@microsoft.com>
2018-01-28 16:55:37 +0100

    ability to avro encode payload

--- routes/telemetry.js
+++ routes/telemetry.js
@@ -1,7 +1,9 @@
 'use strict';
 var env = require('dotenv').config();
-var express = require('express');
+const express = require('express');
 var router = express.Router();
+const avro = require('avro-js');
+const random = require('random-float')
 var util = require('../lib/util');
 
 // azure sdk
@@ -11,9 +13,10 @@ var Client = require('azure-iot-device').Client;
 var Protocol = require('azure-iot-device-mqtt').Mqtt;
 
 var deviceId = 'unknown', devcs = '', hubcs = '', client, status = 'disconnected';
+var sensorArray, payloadType;
 var cs;
 var myTimer, lsm = 'no telemetry started', interval = 60000;
-var sensorArray = [], twinArray = [], sysArray = [], tagArray = [], propArray = [];
+
 
 // auxiliary functions
 function printResultFor(op) {
@@ -23,12 +26,33 @@ function printResultFor(op) {
     };
 }
 
-function composeMessage() {
-    var msg = {};
-    for (var i = 0; i < sensorArray.length; i++)
-        msg[sensorArray[i].name] = Math.random() * (sensorArray[i].max - sensorArray[i].min) + sensorArray[i].min;
+function buildSchema() {
+    var fieldArray = [];
 
-    return msg;
+    for (var i = 0; i < sensorArray.length; i++) {
+        fieldArray[i] = { 'name': sensorArray[i].name, 'type': sensorArray[i].type }
+    }
+
+    payloadType = avro.parse({
+        name: 'payload',
+        type: 'record',
+        fields: fieldArray
+    });
+
+}
+function composeMessage(type) {
+
+    var payload = {};
+
+    for (var i = 0; i < sensorArray.length; i++)
+        payload[sensorArray[i].name] = random(sensorArray[i].max, sensorArray[i].min);
+
+    if (type == 0) { //encode avro
+        var buf = payloadType.toBuffer(payload);
+        return buf
+    }
+    else
+        return payload;
 }
 //routing
 
@@ -39,8 +63,11 @@ router.get('/', function (req, res, next) {
 
 router.post('/', function (req, res, next) {
     var new_lsm = '';
+
     switch (req.body.action) {
         case 'start':
+            sensorArray = util.getSensorArray()
+
             var client = clientFromConnectionString(util.getDev().cs);
             client.open(function (err) {
                 if (err) {
@@ -49,15 +76,17 @@ router.post('/', function (req, res, next) {
                     // Create a message and send it to the IoT Hub at interval
                     if (req.body.interval !== '')
                         interval = req.body.interval;
-                    console.log('setting telemetry at: ' + interval + ' ms');
+                    if (req.body.payload == 0)
+                        buildSchema();
+
                     myTimer = setInterval(function () {
-                        var data = JSON.stringify(composeMessage());
-                        var message = new Message(data);
+                        var payload = JSON.stringify(composeMessage(req.body.payload));
+                        var message = new Message(payload);
+
                         client.sendEvent(message, printResultFor('send'));
                         lsm = new Date(Date.now()).toUTCString();
                         util.setStatus({ 'conn': 'sending telemetry data', 'lsm': lsm })
-                    },
-                        interval);
+                    }, interval);
                     util.setStatus({ 'conn': 'starting to transmit', 'lsm': lsm })
                     res.render('status', { title: 'Azure MQTT telemetry Simulator', status: 'connected', deviceId: util.getDev().deviceId, lsm: 'starting to transmit' });
                 }
@@ -80,9 +109,5 @@ router.post('/', function (req, res, next) {
             break;
     }
 });
-
-
-
-
 
 module.exports = router;
